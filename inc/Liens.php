d46ddc1700fe52561bf1867139c6a8c3e2e81576
AsaaDevCecile <106626342+AsaaDevCecile@users.noreply.github.com>
2022-09-21 16:58:55 +0200

    Update Liens.php

--- inc/Liens.php
+++ inc/Liens.php
@@ -1,1 +1,34 @@
-hjhkh
+<div>
+                <h4><a href="">Centre d'intérêts</a></h4>
+                <ul>
+                    <li><a href="">Epoque médiéval</a></li>
+                    <li><a href="">Culture Asiatique</a></li>
+                </ul>
+            </div>
+            <div>
+                <br>
+                <h4><a href="realisations.php">Réalisations</h4></a>
+                <ul>
+                    <li><a href="rea-creapole.php">Créapole</a></li>
+                    <li><a href="">MJM</a></li>
+                    <li><a href="">Création personnel</li>
+                    <li><a href="">Création média</a></li>
+                    <li><a href="rea-rallye.php">Rallye 2011</a></li>
+                </ul>
+            </div>
+
+            <br>
+
+            <?php require 'inc/asideRoleplay.php'; ?>
+            <br>
+            <div>
+                <h4><a href="contactProfil.php"></a>Contact profil</h4>
+                <ul>
+                    <li><a href="">Fiche de Contact</a></li>
+                    <li><a href="cp-cv.php">Mon CV</a></li>
+                    <li><a href="">Lettre motivation</a></li>
+                    <li><a href="cp-cv-jeux.php">CV pour les jeux</a></li>
+                    <li><a href="cp-cv-coree.php">CV à la coréenne</a></li>
+                </ul>
+            </div>
+
